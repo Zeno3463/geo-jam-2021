66ac5345cf8d2543e4f1acf9d7d53918b9b8655a
Zeno3463 <zenolim3463@gmail.com>
2021-07-19 13:28:34 +0800

    added player's sprite

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -4,17 +4,24 @@ var vel = Vector2.ZERO
 export var speed = 250
 export var gravity = 50
 export var jumpForce = -1000
+var attacking = false
 
 func _physics_process(_delta):
 
 	# horizontal movement
 	if Input.is_action_pressed("left"):
 		vel.x = -speed
+		$body.scale.x = -0.151
 		$Weapon.position = Vector2(-30, 72.028)
+		if not attacking: $AnimationPlayer.play("Walking")
 	elif Input.is_action_pressed("right"):
+		$body.scale.x = 0.151
 		vel.x = speed
 		$Weapon.position = Vector2(30, 72.028)
-	else: vel.x = 0
+		if not attacking: $AnimationPlayer.play("Walking")
+	else: 
+		vel.x = 0
+		if not attacking: $AnimationPlayer.play("idle")
 	
 	# gravity
 	vel.y += gravity
--- scripts/Weapon.gd
+++ scripts/Weapon.gd
@@ -3,10 +3,15 @@ extends Area2D
 var enemies_in_range = null
 
 func _process(delta):
-	if Input.is_action_just_pressed("attack") and enemies_in_range:
-		enemies_in_range.queue_free()
-		enemies_in_range = null
-		
+	if Input.is_action_just_pressed("attack"):
+		get_parent().attacking = true
+		if enemies_in_range:
+			enemies_in_range.queue_free()
+			enemies_in_range = null
+		get_parent().get_node("AnimationPlayer").play("attack")
+		yield(get_parent().get_node("AnimationPlayer"), "animation_finished")
+		get_parent().attacking = false
+
 func _on_Weapon_body_entered(body):
 	if "Enemy" in body.name:
 		enemies_in_range = body
